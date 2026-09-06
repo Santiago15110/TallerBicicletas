3fc03d98656240d9dcb2423a76768da41886708a
Santiago15110 <davids.mamianz@uqvirtual.edu.co>
2026-09-05 20:47:19 -0500

    se agrega los paquetes de modelo, vista, controlador y se crea la clase Taller

--- src/main/java/org/example/taller_bicicletas/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.taller_bicicletas;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
