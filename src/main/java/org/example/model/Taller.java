17714fbaacd878f523001b04f982b50c992c945e
Santiago15110 <davids.mamianz@uqvirtual.edu.co>
2026-09-05 22:11:11 -0500

    nuevos metodos de la clase taller

--- src/main/java/org/example/model/Taller.java
+++ src/main/java/org/example/model/Taller.java
@@ -1,10 +1,13 @@
 package org.example.model;
 
+import java.util.ArrayList;
+
 public class Taller {
 
     private String nombre;
     private String nit;
     private String direccion;
+    private ArrayList<Cliente> listClientes;
 
 
     public Taller(String nombre, String nit, String direeccion) {
@@ -13,9 +16,221 @@ public class Taller {
         this.direccion = direccion;
     }
 
+
+    //Crud Cliente
+
+    //registro cliente
+
+    public boolean registrarCliente(String nombre, String cedula, String telefono, String direccion){
+
+        Cliente newCliente = new Cliente(nombre, cedula, telefono, direccion);
+
+        for(Cliente c: listClientes){
+
+            if(c.getCedula().equals(cedula)){
+                return false;
+            }
+        }
+
+        if(listClientes == null){
+
+        return false;
+        }
+
+        listClientes.add(newCliente);
+        return true;
+
+    }
+
+
+    public boolean eliminarCliente(String cedula){
+
+        Cliente encontrado = buscarClienteByCedula(cedula);
+
+
+
+    }
+
+
+    public Cliente buscarClienteByCedula(String cedula){
+
+        for(Cliente c: listClientes){
+            if(c.getCedula().equals(cedula)){
+                return c;
+            }
+        }
+        return null;
+    }
+
     public String getNombre() {
         return nombre;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
